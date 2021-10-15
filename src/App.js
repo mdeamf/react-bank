4369661deb5f8c77898f942351cf23b642c38161
Maurício Meira <mauriciodealmeidamf@gmail.com>
2021-10-15 09:16:10 -0300

    feat(style): cleaning examples

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 import Header from './components/Header';
@@ -8,18 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
 
-      <div className="App-main">
-        <p>Content</p>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </div>
-
-      <footer className="App-footer">
-        <p>Footer</p>
-      </footer>
     </div>
   );
 }
